2d7d3ebb7b44e5c14b43c4572c38c15193961b3a
Inzulus <inzulus@gmail.com>
2014-11-26 15:20:40 +0100

    Initial commit

---
+++ app/src/main/java/com/inzulus/chessandroid/GameField.java
@@ -0,0 +1,84 @@
+package com.inzulus.chess;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class GameField extends Activity implements View.OnTouchListener{
+
+    int[][] field = new int[7][7];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_field);
+
+        final GridView gridViewBackground = (GridView)findViewById(R.id.gridView1);
+        FieldAdapter fieldAdapter = new FieldAdapter(this);
+        gridViewBackground.setAdapter(fieldAdapter);
+
+        final GridView gridView = (GridView)findViewById(R.id.gridView2);
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+        gridView.setAdapter(imageAdapter);
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(GameField.this, "" + position, Toast.LENGTH_SHORT).show();
+                ((ImageAdapter)gridView.getAdapter()).setFigure(position,1);
+                }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game_field, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        ScaleGestureDetector sgc = new ScaleGestureDetector(this, new ScaleGestureDetector.OnScaleGestureListener() {
+            @Override
+            public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+                return false;
+            }
+
+            @Override
+            public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+                return false;
+            }
+
+            @Override
+            public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+
+            }
+        });
+        sgc.onTouchEvent(motionEvent);
+        return super.onTouchEvent(motionEvent);
+    }
+}
---
+++ app/src/main/java/com/inzulus/chessandroid/ImageAdapter.java
@@ -0,0 +1,70 @@
+package com.inzulus.chess;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Julius on 26.11.2014.
+ */
+public class ImageAdapter extends BaseAdapter{
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return figure_pos.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(0,0,0,0);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(figure_pos[position]);
+        return imageView;
+    }
+
+
+    public void setFigure(int position, int fig_id){
+        figure_pos[position] = figure_id[fig_id];
+        notifyDataSetChanged();
+    }
+
+    private Integer[] figure_id = {
+            R.drawable.wking,R.drawable.wqueen,R.drawable.wbishop,R.drawable.wknight,R.drawable.wrook,R.drawable.wpawn,
+            R.drawable.bking,R.drawable.bqueen,R.drawable.bbishop,R.drawable.bknight,R.drawable.brook,R.drawable.bpawn,
+    };
+
+    private Integer[] figure_pos = {
+            R.drawable.brook,R.drawable.bknight,R.drawable.bbishop,R.drawable.bqueen,R.drawable.bking,R.drawable.bbishop,R.drawable.bknight,R.drawable.brook,
+            R.drawable.bpawn,R.drawable.bpawn,R.drawable.bpawn,R.drawable.bpawn,R.drawable.bpawn,R.drawable.bpawn,R.drawable.bpawn,R.drawable.bpawn,
+            R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,
+            R.drawable.brown, R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white,
+            R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,
+            R.drawable.brown, R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white, R.drawable.brown,R.drawable.white,
+            R.drawable.wpawn,R.drawable.wpawn,R.drawable.wpawn,R.drawable.wpawn,R.drawable.wpawn,R.drawable.wpawn,R.drawable.wpawn,R.drawable.wpawn,
+            R.drawable.wrook,R.drawable.wknight,R.drawable.wbishop,R.drawable.wqueen,R.drawable.wking,R.drawable.wbishop,R.drawable.wknight,R.drawable.wrook,
+
+    };
+}
